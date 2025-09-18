6eac1030c266ba425bdc8c825fc8d51979c70e1e
Anna Karutina <anna.karutina@khk.ee>
2025-09-18 13:48:12 +0300

    Create connection to database

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+dotenv.config();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DRIVER
+});
+
+// Test the database connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 // Sample route
 app.get('/', (req, res) => {
