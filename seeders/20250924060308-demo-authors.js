79d239e8e6a3592ac71fff45d9f82a8b718bd16a
Anna Karutina <anna.karutina@khk.ee>
2025-09-24 09:07:46 +0300

    Created author table and added table data

---
+++ seeders/20250924060308-demo-authors.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Authors', [
+     {
+       name: 'Ashley Galvin',
+       createdAt: new Date(),
+       updatedAt: new Date()
+     }], {});
+     await queryInterface.bulkInsert('Authors', [
+     {
+       name: 'Patrick Beach',
+       createdAt: new Date(),
+       updatedAt: new Date()
+     }], {});
+     await queryInterface.bulkInsert('Authors', [
+     {
+       name: 'MacKenzie Miller',
+       createdAt: new Date(),
+       updatedAt: new Date()
+     }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
